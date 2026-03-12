0e6df5bf5c8e9040c3a804426b3f8dc799a07901
Ricardo Belchior <rbelchior@aalisboa.com.pt>
2026-03-12 10:48:48 +0900

    stadium item component

---
+++ src/components/StadiumItem.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({ item: { type: Object } });
+</script>
+<template>
+  <div>
+    <p :class="{ orphan: item.isOrphan, circular: item.isCircular }">Name: {{ item.Name }}</p>
+    <stadium-item v-for="child in item.children" v-bind:key="child.Id" :item="child"></stadium-item>
+  </div>
+</template>
+<style>
+.orphan {
+  color: blue;
+}
+
+.circular {
+  color: red;
+}
+</style>
